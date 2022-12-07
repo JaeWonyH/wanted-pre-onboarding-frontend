8b32bf72078c21a8616cccea041449c61c6999b9
황재원 <hwangjaewon@hwangjaewon-ui-MacBookAir.local>
2022-12-08 03:59:34 +0900

    feat : [todolists] todolist 가져오기 및 추가

--- front-react/src/components/TodoList.js
+++ front-react/src/components/TodoList.js
@@ -13,33 +13,65 @@ import {
   GetBlogAllTable,
   GetBlogAllTableLine,
 } from "../style/styled";
+import axios from "axios";
 
 export default function TodoList() {
-  const [todoLists, setTodoLists] = useState([
-    {
-      id: 1,
-      todo: "todo2",
-      isCompleted: false,
-      userId: 1,
-    },
-    {
-      id: 2,
-      todo: "todo3",
-      isCompleted: false,
-      userId: 1,
-    },
-  ]);
+  const [todoLists, setTodoLists] = useState([]);
+  const [postListState, setPostListState] = useState(false);
+  const [todo, setTodo] = useState("");
 
-  // useEffect(() => {
-  //     dispatch(getComment(blogId)).then((res) => {
-  //       setTodoLists(res.data)
-  //     });
-  //   }, []);
+  useEffect (()=>{
+    getTodoLists();
+  },[]);
+  
+  async function getTodoLists() {
+    try {
+      axios.defaults.headers.common["Authorization"] = "";
+      const JWTTOEKN = localStorage.getItem("jwtToken");
+      axios.defaults.headers.common["Authorization"] = `Bearer ${JWTTOEKN}`;
+      const res = await axios.get("/todos", {
+        withCredentials: false,
+      });
+      console.log(res.data);
+      setTodoLists(res.data);
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  }
+
+  const todoHandler = (event) => {
+    setTodo(event.currentTarget.value);
+  };
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      axios.defaults.headers.common["Authorization"] = "";
+      const JWTTOEKN = localStorage.getItem("jwtToken");
+      axios.defaults.headers.common["Authorization"] = `Bearer ${JWTTOEKN}`;
+      const res = await axios.post(
+        "/todos",
+        { todo: todo },
+        {
+          withCredentials: false,
+        }
+      );
+      console.log(res.data);
+      getTodoLists();
+      setTodo("");
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  }
+
   const deleteOnClick = () => {
     console.log("삭제 버튼 눌림");
   };
   const updateOnClick = () => {
     console.log("수정 버튼 눌림");
+  };
+  const postOnClick = () => {
+    console.log("등록 버튼 눌림");
+    setPostListState(!postListState);
   };
   const TodoLists = todoLists.map((todoList) => {
     return (
@@ -49,9 +81,7 @@ export default function TodoList() {
             <table>
               <tr>
                 <GetCommentsHeaderBox>
-                  <div>
-                    {todoList.isCompleted ? ("완료") : ("비완료")}
-                  </div>
+                  <div>{todoList.isCompleted ? "완료" : "비완료"}</div>
                   <div></div>
                   <GetBlogAllCreated>
                     <GetCommentsMoreBtn onClick={deleteOnClick}>
@@ -81,6 +111,24 @@ export default function TodoList() {
       <GetBlogAllTable>
         <GetBlogAllTableLine></GetBlogAllTableLine>
         {TodoLists}
+        <GetBlogAllTr>
+          <td>
+            <button onClick={postOnClick}>추가하기</button>
+            {postListState ? (
+              <form onSubmit={onSubmit}>
+                <input
+                  value={todo}
+                  type="text"
+                  placeholder="내용을 입력하세요."
+                  onChange={todoHandler}
+                ></input>
+                <button>등록하기</button>
+              </form>
+            ) : (
+              <div></div>
+            )}
+          </td>
+        </GetBlogAllTr>
       </GetBlogAllTable>
     </div>
   );
