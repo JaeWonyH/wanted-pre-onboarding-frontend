fb176c9f32fab39850f97ba155669ca958153a37
황재원 <hwangjaewon@hwangjaewon-ui-MacBookAir.local>
2022-12-12 19:08:13 +0900

    feat:[deleteTodo] 삭제 기능

--- front-react/src/components/TodoDetail.js
+++ front-react/src/components/TodoDetail.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState } from "react";
 import moreBtn from "../img/moreBtn.png";
 import deleteImg from "../img/delete.png";
 import editImg from "../img/edit.png";
@@ -10,20 +10,32 @@ import {
   GetCommentsMoreBtn,
   DeleteCommentImg,
   GetBlogAllContent,
-  GetBlogAllTable,
-  GetBlogAllTableLine,
-  PostBlogBtn,
-  PostCommentInput,
 } from "../style/styled";
+import axios from "axios";
 
-export default function TodoDetail({todoList}) {
-    const deleteOnClick = () => {
-        console.log("삭제 버튼 눌림");
-      };
-      const updateOnClick = () => {
-        console.log("수정 버튼 눌림");
-      };
-      console.log(todoList);
+export default function TodoDetail({ todoList }) {
+  const [updateState, setUpdateState] = useState(false);
+  async function deleteTodo() {
+    try {
+      axios.defaults.headers.common["Authorization"] = "";
+      const JWTTOEKN = localStorage.getItem("jwtToken");
+      axios.defaults.headers.common["Authorization"] = `Bearer ${JWTTOEKN}`;
+      const res = await axios.delete(`/todos/${todoList.id}`, {
+        withCredentials: false,
+      });
+      window.location.reload();
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  }
+  const deleteOnClick = () => {
+    console.log("삭제 버튼 눌림");
+    deleteTodo();
+  };
+  const updateOnClick = () => {
+    console.log("수정 버튼 눌림");
+    setUpdateState(!updateState);
+  };
   return (
     <GetBlogAllTr>
       <td>
