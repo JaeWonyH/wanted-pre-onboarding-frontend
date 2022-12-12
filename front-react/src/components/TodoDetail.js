10ec55560aa5bee9b1ca58f07d6819baff314633
황재원 <hwangjaewon@hwangjaewon-ui-MacBookAir.local>
2022-12-12 19:55:14 +0900

    feat : todo 수정 기능

--- front-react/src/components/TodoDetail.js
+++ front-react/src/components/TodoDetail.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import moreBtn from "../img/moreBtn.png";
 import deleteImg from "../img/delete.png";
 import editImg from "../img/edit.png";
@@ -10,11 +10,36 @@ import {
   GetCommentsMoreBtn,
   DeleteCommentImg,
   GetBlogAllContent,
+  PostBlogBtn,
+  PostCommentInput,
 } from "../style/styled";
 import axios from "axios";
 
 export default function TodoDetail({ todoList }) {
   const [updateState, setUpdateState] = useState(false);
+  const [isCompletedState, setIsCompletedState] = useState(
+    todoList.isCompleted
+  );
+  const [todoState, setTodoState] = useState(todoList.todo);
+  const [todo, setTodo] = useState({
+    todo: String,
+    isCompleted: Boolean,
+  });
+  const todoHandler = (event) => {
+    setTodoState(event.currentTarget.value);
+    setTodo({
+      isCompleted: isCompletedState,
+      todo: event.currentTarget.value,
+    });
+  };
+  const isCompletedHandler = (event) => {
+    console.log(event.currentTarget.checked);
+    setIsCompletedState(event.currentTarget.checked);
+    setTodo({
+      isCompleted: event.currentTarget.checked,
+      todo: todoState,
+    });
+  };
   async function deleteTodo() {
     try {
       axios.defaults.headers.common["Authorization"] = "";
@@ -28,6 +53,32 @@ export default function TodoDetail({ todoList }) {
       console.log(error.response.data.message);
     }
   }
+  async function putTodo() {
+    try {
+      axios.defaults.headers.common["Authorization"] = "";
+      const JWTTOEKN = localStorage.getItem("jwtToken");
+      axios.defaults.headers.common["Authorization"] = `Bearer ${JWTTOEKN}`;
+      const res = await axios.put(
+        `/todos/${todoList.id}`,
+        { todo: todo.todo, isCompleted: todo.isCompleted },
+        {
+          withCredentials: false,
+        }
+      );
+        window.location.reload();
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  }
+  const onSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+      console.log(todo);
+      putTodo();
+    },
+    [isCompletedState, todoState, todo, isCompletedHandler, todoHandler]
+  );
+
   const deleteOnClick = () => {
     console.log("삭제 버튼 눌림");
     deleteTodo();
@@ -63,6 +114,37 @@ export default function TodoDetail({ todoList }) {
             </tr>
           </table>
         </GetCommentsDiv>
+        {updateState ? (
+          <td>
+            <form onSubmit={onSubmit}>
+              <PostCommentInput
+                value={todoState}
+                type="text"
+                placeholder="내용을 입력하세요."
+                onChange={todoHandler}
+              ></PostCommentInput>
+              {isCompletedState ? (
+                <input
+                  value={isCompletedState}
+                  type="checkbox"
+                  onChange={isCompletedHandler}
+                  checked
+                ></input>
+              ) : (
+                <input
+                  value={isCompletedState}
+                  type="checkbox"
+                  onChange={isCompletedHandler}
+                ></input>
+              )}
+
+              <PostBlogBtn>수정하기</PostBlogBtn>
+              {/* <PostBlogBtn>취소하기</PostBlogBtn> */}
+            </form>
+          </td>
+        ) : (
+          <div></div>
+        )}
       </td>
     </GetBlogAllTr>
   );
